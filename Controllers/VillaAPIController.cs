668536158a7781880360879ceb47d7943fa4e3f2
Caleb McCann <mccanncm66@gmail.com>
2024-02-07 20:12:08 -0800

    Add HttpPatch

--- Controllers/VillaAPIController.cs
+++ Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
 using RestfulAPI.Data;
 using RestfulAPI.Models;
 using RestfulAPI.Models.Dto;
@@ -100,6 +101,30 @@ namespace RestfulAPI.Controllers
 
             return NoContent();
 
+        }
+
+        [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDTO> patchDTO) 
+        { 
+            if( patchDTO == null || id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if(villa == null)
+            {
+                return BadRequest();
+            }
+
+            patchDTO.ApplyTo(villa, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return NoContent();
         }
     }
 }
