44dc1073d02c2ea5c1a5703008956bc2f29de796
Caleb McCann <mccanncm66@gmail.com>
2024-02-07 20:03:40 -0800

    Add HttpPost and HttpPut

--- Controllers/VillaAPIController.cs
+++ Controllers/VillaAPIController.cs
@@ -2,6 +2,7 @@
 using RestfulAPI.Data;
 using RestfulAPI.Models;
 using RestfulAPI.Models.Dto;
+using System.Security.Principal;
 
 namespace RestfulAPI.Controllers
 {
@@ -15,8 +16,8 @@ namespace RestfulAPI.Controllers
             return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("{id:int}", Name ="GetVilla")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(VillaDTO))]
+        [HttpGet("{id:int}", Name = "GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(VillaDTO))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         //[ProducesResponseType(200, Type = typeof(VillaDTO))]
@@ -24,7 +25,7 @@ namespace RestfulAPI.Controllers
         //[ProducesResponseType(404)]
         public ActionResult<VillaDTO> GetVilla(int id)
         {
-            if(id == 0)
+            if (id == 0)
             {
                 return BadRequest();
             }
@@ -39,29 +40,66 @@ namespace RestfulAPI.Controllers
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO) 
+        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
         {
             //when not an [ApiController]
             //if (!ModelState.IsValid)
             //{
             //    return BadRequest(ModelState);
             //}
-            if(VillaStore.villaList.FirstOrDefault(u=> u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
             {
                 ModelState.AddModelError("", "Villa already Exists!");
                 return BadRequest(ModelState);
             }
-            if(villaDTO == null)
+            if (villaDTO == null)
             {
                 return BadRequest(villaDTO);
             }
-            if(villaDTO.Id > 0)
+            if (villaDTO.Id > 0)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
             villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
             VillaStore.villaList.Add(villaDTO);
             return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            VillaStore.villaList.Remove(villa);
+            return NoContent();
+        }
+
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult UpdateVilla(int id, [FromBody]VillaDTO villaDTO)
+        {
+            if(villaDTO == null || id != villaDTO.Id)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            villa.Name = villaDTO.Name;
+            villa.Sqft = villaDTO.Sqft;
+            villa.Occupancy = villaDTO.Occupancy;
+
+            return NoContent();
+
         }
     }
 }
--- Data/VillaStore.cs
+++ Data/VillaStore.cs
@@ -6,8 +6,8 @@ namespace RestfulAPI.Data
     {
         public static List<VillaDTO> villaList = new List<VillaDTO>
         {
-            new VillaDTO { Id = 1, Name = "Pool View" },
-            new VillaDTO { Id = 2, Name = "Beach Views" }
+            new VillaDTO { Id = 1, Name = "Pool View", Sqft=100, Occupancy=4 },
+            new VillaDTO { Id = 2, Name = "Beach Views", Sqft=300, Occupancy=3 }
         };
     }
 }
